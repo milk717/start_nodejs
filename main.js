cc833098875d1fa3ef1733372361ad2e50337508
milk717 <robolindasoo@gmail.com>
2022-06-16 23:47:10 +0900

    파일리스트 불러오기

--- main.js
+++ main.js
@@ -7,32 +7,60 @@ var app = http.createServer(function(request, response){
     var queryData = (url).parse(_url,true).query;
     var pathname = url.parse(_url,true).pathname;
     var title = queryData.id;
-    if(pathname==='/'){
-        fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
-            console.log(description);
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              <ul>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-              </ul>
-              <h2>${title}</h2>
-              <p>${description}</p>
-              </body>
-            </html>
-        `
-            response.writeHead(200);
-            response.end(template);
-        });
-    }else{
+    if(pathname==='/'){ //루트일때
+        if(title === undefined){    //홈일때
+            fs.readdir('./data',function (error,filelist){
+                console.log(filelist);
+                var list = '<ul>';
+                for(var i = 0; i<filelist.length; i++){
+                    list += `<li><a href="${filelist[i]}"></a>${filelist[i]}</li>`;
+                }
+                list += '</ul>';
+                var template = `
+                <!doctype html>
+                    <html>
+                    <head>
+                      <title>WEB1 - Welcome</title>
+                      <meta charset="utf-8">
+                    </head>
+                    <body>
+                      <h1><a href="/">WEB</a></h1>
+                      ${list}
+                      <h2>Welcome</h2>
+                      <p>hello node.js</p>
+                      </body>
+                    </html>
+                 `
+                response.writeHead(200);
+                response.end(template);
+            })
+        }else{
+            fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+                console.log(description);
+                var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                      <title>WEB1 - ${title}</title>
+                      <meta charset="utf-8">
+                    </head>
+                    <body>
+                      <h1><a href="/">WEB</a></h1>
+                      <ul>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                      </ul>
+                      <h2>${title}</h2>
+                      <p>${description}</p>
+                      </body>
+                    </html>
+                  `
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    }else{ //에러날때
         response.writeHead(404);
         response.end('Not found');
     }
@@ -50,6 +78,5 @@ var app = http.createServer(function(request, response){
     //fs.readFileSync => 경로를 읽어서 응답함
     // response.end(fs.readFileSync(__dirname+_url));
     // response.end(queryData.id);
-
 });
 app.listen(3000);
