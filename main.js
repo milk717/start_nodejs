f62eb88d3f53daec495a489c42dd0e62c1bf8fbd
milk717 <robolindasoo@gmail.com>
2022-06-17 13:11:01 +0900

    파일리스트도 함수로

--- main.js
+++ main.js
@@ -20,56 +20,44 @@ function templateHTML(title, list, description){
           `;
 }
 
+function templateList(filelist){
+    var list = '<ul>';
+    for(var i = 0; i<filelist.length; i++){
+        list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    }
+    list += '</ul>';
+    return list;
+}
+
 var app = http.createServer(function(request, response){
     var _url = request.url;
     var queryData = (url).parse(_url,true).query;
     var pathname = url.parse(_url,true).pathname;
     var title = queryData.id;
-    if(pathname==='/'){ //루트일때
-        if(title === undefined){    //홈일때
-            fs.readdir('./data',function (error,filelist){
-                var list = '<ul>';
-                for(var i = 0; i<filelist.length; i++){
-                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                }
-                list += '</ul>';
-                console.log(list);
-                var template = templateHTML("Welcome",list,"Hello node.js!");
+    var list;
+
+    fs.readdir('./data',function (error,filelist){
+        list = templateList(filelist);
+        console.log(list);  //List Value Exists
+    });
+
+    if(pathname==='/'){ //root
+        if(title === undefined){    //home
+            console.log(list);      //undefined
+            var template = templateHTML("Welcome",list,"Hello node.js!");
+            response.writeHead(200);
+            response.end(template);
+        }else{
+            fs.readFile(`data/${title}`,'utf-8',function (err, description){
+                console.log(list);  //List Value Exists
+                var template = templateHTML(title,list,description);
                 response.writeHead(200);
                 response.end(template);
-            })
-        }else{
-            fs.readdir('./data',function (error,filelist){
-                var list = '<ul>';
-                for(var i = 0; i<filelist.length; i++){
-                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                }
-                list += '</ul>';
-                fs.readFile(`data/${title}`,'utf-8',function (err, description){
-                    var template = templateHTML(title,list,description);
-                    response.writeHead(200);
-                    response.end(template);
-                });
-
             });
         }
-    }else{ //에러날때
+    }else{ //error
         response.writeHead(404);
         response.end('Not found');
     }
-    // if(_url=='/'){
-    //     title='Welcome';
-    // }
-    // if(_url=='favicon.ico'){
-    //     response.writeHead(404);
-    //     response.end();
-    //     return;
-    // }
-
-    //console.log(__dirname+_url); //경로
-    //response.end => 웹 서버가 브라우저의 요청에 응답하는 명령
-    //fs.readFileSync => 경로를 읽어서 응답함
-    // response.end(fs.readFileSync(__dirname+_url));
-    // response.end(queryData.id);
 });
-app.listen(3000);
+app.listen(3000);   //port number
